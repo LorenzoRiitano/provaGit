523d1aaf04a3fd201126ddefdea0ec775dc73ebc
Cristina <Cristina@LAPTOP-TIRO0GQH.homenet.telecomitalia.it>
2021-04-09 16:07:20 +0200

    esercizio ricorsione(CON STRINGHE)

---
+++ src/main/java/it/polito/tdp/anagram/model/Anagram.java
@@ -0,0 +1,50 @@
+package it.polito.tdp.anagram.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Anagram {
+
+	public List<String> anagrammi(String parola) {
+		List<String> risultato = new ArrayList<>() ;
+		permuta("", parola, 0,  risultato) ; // LANCIA la ricorsione
+		return null ;
+		
+	}
+	
+	// livello = lunghezza della soluzione parziale
+	// livello iniziale = 0
+	// parziale = stringa che contiene l'anagramma incompleto in fase di costruzione
+	// lettere = le lettere della parola iniziale che ancora non abbiamo utilizzato
+	//           (=== il sotto-problema che dobbiamo risolvere)
+	
+	private void permuta(String parziale, String lettere, int livello, List<String> risultato) {
+		if( lettere.length()==0 ) { // caso terminale 
+			// la soluzione parziale Ã¨ anche una soluzione completa!!
+
+			// if(parziale Ã¨ una parola valida?) {
+			risultato.add(parziale) ;
+		} else {
+			// fai ricorsione
+			// sottoproblema == una lettera (un singolo carattere) di 'lettere'
+			for(int pos=0; pos<lettere.length(); pos++) {
+				
+				char tentativo = lettere.charAt(pos) ; 
+				
+				String nuovaParziale = parziale + tentativo ;
+				String nuovaLettere = lettere.substring(0, pos)+lettere.substring(pos+1) ;
+					// togli il carattere pos da lettere
+				
+				// if (nuovaParziale Ã¨ un PREFISSO valido di almeno una parola nel dizionario)
+				// "aqz" -> no ; "car" -> si (carro, carrello, carta...)
+				permuta(nuovaParziale, nuovaLettere, livello+1, risultato) ;
+				
+				// Backtracking (NON SERVE)
+				// rimetti a posto 'parziale'
+				// rimetti a posto 'lettere'
+				
+			}
+		}
+	}
+	
+}
